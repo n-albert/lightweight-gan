1a4a32b16919ef7bd07d5355d538c001dc9bf0f8
Nikita Albert <61245089+n-albert@users.noreply.github.com>
2021-12-12 22:24:50 -0500

    Add files via upload

--- lightweight_gan/lightweight_gan.py
+++ lightweight_gan/lightweight_gan.py
@@ -337,6 +337,8 @@ class ImageDataset(Dataset):
             img = Image.open(path)
             img = img.resize((self.image_size, self.image_size))
             img = np.array(img)
+            print(img.shape())
+            print(index)
             self.cached_images[index, :, :, :] = img
             self.cached_true[index] = 1
                 
